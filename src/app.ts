01f616d9ee244ff64b40c446c7cd7d55fbd881b5
Patrik Balvan <ba.patko@gmail.com>
2023-12-06 22:37:16 +0100

    .

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,43 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import Article from './models/article.ts'
+
+const dbURI = 'mongodb://localhost:27017/VAII'
 
 const app = express()
+mongoose.connect(dbURI)
+    .then(() => {
+        console.log('connected to db')
+        app.listen(5000, () => {
+            console.log('server started')
+        })
+    })
+    .catch((err) => {
+        console.error(err)
+    })
 
-app.listen(5000, () => {
-    console.log('server started')
+app.get('/get_blog', (req, res) => {
+    console.log('get')
+    Article.find()
+        .then((result) => {
+            res.json(result)
+        })
+        .catch((err) => {
+            console.error(err)
+        })
 })
 
-app.get('/api', (req, res) => {
-    console.log('api request!')
-    res.json({"users" : ['two', 'three', 'four']})
+app.get('/save_blog', (req, res) => {
+    const article = new Article({
+        title: 'Title1',
+        content: "content1"
+    })
+
+    article.save()
+        .then(() => {
+            console.log('saved')
+        })
+        .catch((err) => {
+            console.error(err)
+        })
 })
